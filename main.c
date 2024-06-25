d95b4a83cd5f13eebedcc9ed42bdf987ba786c51
Heeral Dulobdas <zkac317@live.rhul.ac.uk>
2024-06-25 12:31:01 +0100

    added menu

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+char string[20];
+
+printf("Enter string: ");
+scanf("%s", string);
+
+printf("1. Encrypt\n2. Decrypt");
+int choice = getchar();
+}
+
